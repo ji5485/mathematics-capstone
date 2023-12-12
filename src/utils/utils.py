d1ab4857125e5e0fe696a25e0bf10baf945bbc69
gg-biny <118068193+gg-biny@users.noreply.github.com>
2023-12-12 14:25:46 +0900

    Update utils.py
    Add create scene directory function

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -6,3 +6,11 @@ def createDirectory(requestId):
       os.makedirs(f"{os.getcwd()}/artifacts/{requestId}")
   except OSError:
     pass
+    
+def createSceneDirectory():
+
+  path = f"{os.getcwd()}/{director_chat}/{scene_number}"
+
+  for scene_number in range(1, total_scene + 1):
+    os.makedirs(path,exist_ok=True)
+    os.chdir(path)
