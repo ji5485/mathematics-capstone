e3598a807ca3eba2907ea0d481246f95980a2733
gg-biny <118068193+gg-biny@users.noreply.github.com>
2023-12-13 18:24:10 +0900

    Add textToAudio.py

---
+++ src/packages/models/tta.py
@@ -0,0 +1,22 @@
+!nvidia-smi
+!python3 -m pip install -U git+https://github.com/facebookresearch/audiocraft#egg=audiocraft
+
+from audiocraft.models import musicgen
+from audiocraft.utils.notebook import display_audio
+
+class TextToAudio:
+    def __init__(self):
+        # 모델 초기화
+        self.model = musicgen.MusicGen.get_pretrained('facebook/musicgen-small', device='cuda')
+        self.model.generation_params = {'use_sampling': True,
+                                        'temp': 1.0,
+                                        'top_k': 250,
+                                        'top_p': 0}
+        self.model.set_generation_params(duration=5)
+
+    # 스크립트를 받아 음악을 생성하는 메서드
+    def textToAudio(self, script):
+        self.script = script
+        output_music = self.model.generate([script], progress=True)
+
+        return output_music
